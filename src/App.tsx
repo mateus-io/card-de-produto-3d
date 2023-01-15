3ff14327abff43a44677403ec6cdc97264cfa9b4
mateus-coder1 <letsgood56@gmail.com>
2023-01-15 17:48:38 -0300

    :sparkles: Feat: TASK-004 Render Product Card

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { ProductCard } from './components/ProductCard/ProductCard';
+
+export default function App() {
+  return (
+    <main
+      className="Content"
+    >
+      <ProductCard />
+    </main>
+  );
+}

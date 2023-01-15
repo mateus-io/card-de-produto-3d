870262d8797bbfe035e7228fc885130f02c1be05
mateus-coder1 <letsgood56@gmail.com>
2023-01-15 17:47:24 -0300

    :bento: Assets: TASK-002 Add assets

---
+++ src/assets/Assets.ts
@@ -0,0 +1,5 @@
+import tridimensionalVisualizationIcon from './360degreeicon.png';
+import closeIcon from './closeicon.png';
+import staticSofa from './sofa.png';
+
+export { tridimensionalVisualizationIcon, closeIcon, staticSofa };
